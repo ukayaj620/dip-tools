1e6ef794f2a1c4e4a1a812255a1da74c8604b6a2
gregetexe <53731848+gregetexe@users.noreply.github.com>
2020-09-29 22:55:12 +0700

    feat: finished second control tab UI

--- backup/main.pas
+++ backup/main.pas
@@ -13,43 +13,77 @@ type
   { TDIPTools }
 
   TDIPTools = class(TForm)
+    blueButton: TButton;
     blueGSButton: TButton;
     binaryButton: TButton;
-    binaryOptionsPanel: TPanel;
+    binarySettingsPanel: TPanel;
     brightnessGroup: TGroupBox;
-    redBinaryButton: TButton;
-    greenBinaryButton: TButton;
-    blueBinaryButton: TButton;
+    binaryFiltersGroup: TGroupBox;
+    binaryRedButton: TButton;
+    binaryGreenButton: TButton;
+    binaryBlueButton: TButton;
+    inverseButton: TButton;
+    inverseGroup: TGroupBox;
+    sketchButton: TButton;
+    executeButton: TButton;
+    HPL0Radio: TRadioButton;
+    HPL1Radio: TRadioButton;
+    kernelValue: TEdit;
+    kernelLabel: TLabel;
+    brightnessOptionsPanel: TPanel;
+    brightnessLabel: TLabel;
+    brightnessButton: TButton;
+    contrastButton: TButton;
+    colorGroup: TGroupBox;
+    colorPanel: TPanel;
+    imagealterGroup: TGroupBox;
+    gValueLabel: TLabel;
+    gValueIndicator: TEdit;
+    greenButton: TButton;
+    enhancementPanel: TPanel;
+    contrastGroup: TGroupBox;
+    colorRadioGroup: TRadioGroup;
+    colorRadioButton: TRadioButton;
+    grayscaleRadioButton: TRadioButton;
+    filterRadioGroup: TRadioGroup;
+    LPLRadio: TRadioButton;
+    methodRadioGroup: TRadioGroup;
+    correlationRadio: TRadioButton;
+    convolutionRadio: TRadioButton;
     grayscaleGroup: TGroupBox;
     grayscaleButton: TButton;
-    binaryGroup: TGroupBox;
+    binarySettingsGroup: TGroupBox;
     greenGSButton: TButton;
+    redButton: TButton;
     resetButton: TButton;
     redGSButton: TButton;
-    colorControlPanel: TTabControl;
     OpenPictureDialog: TOpenPictureDialog;
     saveFileButton: TButton;
     openFileButton: TButton;
-    colorGroup: TGroupBox;
     fileGroup: TGroupBox;
-    redButton: TButton;
-    greenButton: TButton;
-    blueButton: TButton;
     originalImage: TImage;
     SavePictureDialog: TSavePictureDialog;
-    enhancementControlPanel: TTabControl;
     targetImage: TImage;
     colorToggle: TToggleBox;
     enhanceToggle: TToggleBox;
     thresholdIndicator: TEdit;
+    brightnessIndicator: TEdit;
+    contrastIndicator: TEdit;
     thresholdLabel: TLabel;
+    contrastLabel: TLabel;
     thresholdTrackbar: TTrackBar;
+    brightnessTrackbar: TTrackBar;
+    contrastTrackbar: TTrackBar;
+    gValueTrackbar: TTrackBar;
     procedure enhanceToggleChange(Sender: TObject);
     procedure colorToggleChange(Sender: TObject);
+    procedure gValueTrackbarChange(Sender: TObject);
     procedure openFileButtonClick(Sender: TObject);
     procedure resetButtonClick(Sender: TObject);
     procedure saveFileButtonClick(Sender: TObject);
     procedure thresholdTrackbarChange(Sender: TObject);
+    procedure brightnessTrackbarChange(Sender: TObject);
+    procedure contrastTrackbarChange(Sender: TObject);
   private
 
   public
@@ -58,6 +92,10 @@ type
 
 var
   DIPTools: TDIPTools;
+
+  imageWidth: Integer;
+  imageHeight: Integer;
+
   bitmapR: array [0..1000, 0..1000] of Byte;
   bitmapG: array [0..1000, 0..1000] of Byte;
   bitmapB: array [0..1000, 0..1000] of Byte;
@@ -66,9 +104,9 @@ var
   bitmapTempG: array [0..1000, 0..1000] of Byte;
   bitmapTempB: array [0..1000, 0..1000] of Byte;
 
-  bitmapInversR : array [0..1000, 0..1000] of Byte;
-  bitmapInversG : array [0..1000, 0..1000] of Byte;
-  bitmapInversB : array [0..1000, 0..1000] of Byte;
+  bitmapInversR: array [0..1000, 0..1000] of Byte;
+  bitmapInversG: array [0..1000, 0..1000] of Byte;
+  bitmapInversB: array [0..1000, 0..1000] of Byte;
 
   bitmapBiner: array[0..1000, 0..1000] of Boolean;
 
@@ -89,6 +127,12 @@ begin
   begin
     originalImage.Picture.LoadFromFile(OpenPictureDialog.FileName);
     targetImage.Picture.LoadFromFile(OpenPictureDialog.FileName);
+
+    imageWidth:= originalImage.Width;
+    imageHeight:= originalImage.Height;
+
+    targetImage.Width:= imageWidth;
+    targetImage.Height:= imageHeight;
 
     for y := 0 to originalImage.Height - 1 do
     begin
@@ -146,35 +190,60 @@ end;
 //Color Options
 procedure TDIPTools.colorToggleChange(Sender: TObject);
 begin
-  if colorControlPanel.Visible = true then
+  enhanceToggle.Checked:= false;
+
+  if colorPanel.Visible = true then
   begin
-    colorControlPanel.Visible:= false;
-    enhancementControlPanel.Visible:= false;
+    colorPanel.Visible:= false;
+    enhancementPanel.Visible:= false;
   end
      else
      begin
-       colorControlPanel.Visible:= true;
+       colorPanel.Visible:= true;
+       enhancementPanel.Visible:= false;
+       thresholdIndicator.Text:= IntToStr(thresholdTrackbar.Position);
      end;
 end;
 
-//Alteration Options
+//Enhancement Options
 procedure TDIPTools.enhanceToggleChange(Sender: TObject);
 begin
-  if enhancementControlPanel.Visible = true then
+  colorToggle.Checked:= false;
+
+  if enhancementPanel.Visible = true then
   begin
-    enhancementControlPanel.Visible:= false;
+    colorPanel.Visible:= false;
+    enhancementPanel.Visible:= false;
   end
      else
      begin
-       colorControlPanel.Visible:= false;
-       enhancementControlPanel.Visible:= true;
+       colorPanel.Visible:= false;
+       enhancementPanel.Visible:= true;
+       brightnessIndicator.Text:= IntToStr(brightnessTrackbar.Position);
+       contrastIndicator.Text:= IntToStr(contrastTrackbar.Position);
+       gValueIndicator.Text:= IntToStr(gValueTrackbar.Position);
      end;
 end;
 
-//Binary Trackbar
+//Trackbars
 procedure TDIPTools.thresholdTrackbarChange(Sender: TObject);
 begin
   thresholdIndicator.Text:= IntToStr(thresholdTrackbar.Position);
+end;
+
+procedure TDIPTools.brightnessTrackbarChange(Sender: TObject);
+begin
+  brightnessIndicator.Text:= IntToStr(brightnessTrackbar.Position);
+end;
+
+procedure TDIPTools.contrastTrackbarChange(Sender: TObject);
+begin
+  contrastIndicator.Text:= IntToStr(contrastTrackbar.Position);
+end;
+
+procedure TDIPTools.gValueTrackbarChange(Sender: TObject);
+begin
+  gValueIndicator.Text:= IntToStr(gValueTrackbar.Position);
 end;
 end.
 
